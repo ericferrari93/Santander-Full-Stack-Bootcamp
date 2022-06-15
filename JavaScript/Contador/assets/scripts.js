8006d92d1a63400de8ca931a1f817d99220667af
Eric Ferrari Ramos <ericferrarir1@gmail.com>
2022-06-15 20:27:59 -0300

    Exercicios JS

---
+++ JavaScript/Contador/assets/scripts.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+const current_number = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	current_number.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	current_number.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
